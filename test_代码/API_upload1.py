f97a0d95b61670d16e177069f06fbc58cf2a9681
mazhenwei <471102351@qq.com>
2018-12-10 09:17:04 +0800

    Create API_upload1.py

---
+++ test_代码/API_upload1.py
@@ -0,0 +1,25 @@
+from urllib.parse import urljoin
+import json
+import os
+import requests
+web="http://10.0.200.2:8000"
+test_API="/api/v1/auth-token/"
+#获取token
+post_text=requests.post(urljoin(web,test_API),data={'version':'v1','username':'mazhenwei@cnic.cn','password':'mazhenwei'})
+text=json.loads(post_text.text)
+token=text['token']
+#创建一个桶
+#bucket_post=requests.post(urljoin(web,'/api/v1/buckets/'),data={'name':'string11'},headers= {'Authorization':'Token '+token})
+##print(bucket_post.text)
+#待测试的文件
+files={'chunk':open('02f997dc6dbd3354a3478e6109761194.png','rb')}
+fsize=os.path.getsize('./02f997dc6dbd3354a3478e6109761194.png')
+
+#循环上传1m图片
+for i in range(1,10000001):
+    r="%d" %i
+    files={'chunk':open('02f997dc6dbd3354a3478e6109761194.png','rb')}
+    upload_API=requests.put(urljoin(web,'/api/v1/obj/sss/'+r+'/'),files=files, data={"chunk_offset": 0,"chunk_size":fsize},headers= {'Authorization':'Token '+token})
+    new=upload_API.text
+    new1=json.loads(new)
+    print(new1)   
